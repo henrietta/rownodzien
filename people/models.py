337d5e55f99f3c560801fea9c25b21577becb938
henrietta <piotr.maslanka@henrietta.com.pl>
2013-03-01 22:28:13 +0100

    Zmieniono wymaganie pola PESEL
    Pole PESEL musi być unikatowe

--- people/models.py
+++ people/models.py
@@ -5,7 +5,7 @@ class Reader(models.Model):
     """Ta tabela określa czytelników obecnych w systemie"""
     number = models.DecimalField(max_digits=4, decimal_places=0, unique=True, verbose_name=u'Numer czytelnika')
 
-    pesel = models.CharField(max_length=10, verbose_name=u'PESEL')
+    pesel = models.CharField(max_length=11, verbose_name=u'PESEL', unique=True)
     name = models.CharField(max_length=50, verbose_name=u'Imię')
     surname = models.CharField(max_length=50, verbose_name=u'Nazwisko')
     phone = models.CharField(max_length=30, verbose_name=u'Telefon')
