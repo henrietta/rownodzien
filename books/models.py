ab39c6145ed4e876ec491259e3b9ef1e50486e3c
henrietta <piotr.maslanka@henrietta.com.pl>
2013-03-01 21:18:16 +0100

    Zmiana wymagań pola code w tabeli BookInstance
    Pole code musi być teraz unikatowe

--- books/models.py
+++ books/models.py
@@ -18,7 +18,7 @@ class BookInstance(models.Model):
     """Ta tabela określa egzemplarze książek dostępne w systemie"""
     book = models.ForeignKey(Book, related_name='instances', verbose_name=u'Książka')
 
-    code = models.CharField(max_length=14, verbose_name=u'Nr lub kod kreskowy')
+    code = models.CharField(max_length=14, unique=True, verbose_name=u'Nr lub kod kreskowy')
 
     is_damaged = models.BooleanField(default=False, verbose_name=u'Czy uszkodzona?')
     is_lost = models.BooleanField(default=False, verbose_name=u'Czy zagubiona?')
