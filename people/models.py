bd7300b45c2df8a3f1e674e24d52e93f009a0ab8
henrietta <piotr.maslanka@henrietta.com.pl>
2013-03-01 08:26:58 +0100

    Korekta modeli people
    Wymagane było podane wartości decimal_places. Zostało to uczynione.

--- people/models.py
+++ people/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Reader(models.Model):
     """Ta tabela określa czytelników obecnych w systemie"""
-    number = models.DecimalField(max_digits=4, verbose_name=u'Numer czytelnika')
+    number = models.DecimalField(max_digits=4, decimal_places=0, verbose_name=u'Numer czytelnika')
 
     pesel = models.CharField(max_length=10, verbose_name=u'PESEL')
     name = models.CharField(max_length=50, verbose_name=u'Imię')
@@ -12,7 +12,7 @@ class Reader(models.Model):
 
 class Librarian(models.Model):
     """Ta tabela określa bibliotekarzy obecnych w systemie"""
-    number = models.DecimalField(max_digits=4, verbose_name=u'Numer czytelnika')
+    number = models.DecimalField(max_digits=4, decimal_places=0, verbose_name=u'Numer bibliotekarza')
 
     name = models.CharField(max_length=50, verbose_name=u'Imię')
     surname = models.CharField(max_length=50, verbose_name=u'Nazwisko')
