534a4b98994d1dd4bf47c6164a3bfcc7b7c4b15d
henrietta <piotr.maslanka@henrietta.com.pl>
2013-03-03 22:02:47 +0100

    Powiązano transakcje z kontrolerami
    Teraz działanie kontrolera będzie odbywać się w ramach jednej transakcji
    bazy danych

--- settings.py
+++ settings.py
@@ -75,6 +75,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
